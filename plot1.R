86508360b401ca6fe6bc818a3054421770deb93d
CarlitaM <carlamusitrev@gmail.com>
2016-04-24 17:39:36 +0200

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -13,6 +13,6 @@ rm(data)
 
 ## Create Plot 1 
 
-png("plot1.png", width=480, height=480)
+png("plot1.png", width = 480, height = 480)
 hist(subsetdata$Global_active_power, main = "Global Active Power", xlab = "Global Active Power (kilowatts)", ylab = "Frequency", col = "Red")
 dev.off()
